ae13ab3621646e5ca7b35e0cca9c94b5a0c87ebd
vitor0p9f <dev.vitorpfontes002@gmail.com>
2025-08-29 21:12:12 -0300

    feat(core/artifacts/events): atualiza o evento genérico de start
    Atualiza `StartedEvent` para inlcuir um título para a tarefa.

--- seisbai_contracs/core/artefacts/events/started.py
+++ seisbai_contracs/core/artefacts/events/started.py
@@ -5,16 +5,24 @@ class StartedEvent(Event, frozen=True, kw_only=True):
     """
     Evento que representa o início de uma tarefa no sistema.
 
-    Esse evento é emitido quando uma nova tarefa é iniciada,
-    permitindo rastrear sua execução desde o momento em que começou.
-    Ele herda de `Event`, garantindo que possua os campos
-    `id`, `timestamp` e `message`.
+    Este evento é emitido assim que uma nova tarefa é iniciada,
+    permitindo rastrear sua execução desde o ponto inicial.
+    Ele herda de `Event`, o que garante que também possua os
+    campos genéricos `id`, `timestamp` e `message` utilizados
+    para padronizar o sistema de eventos.
 
     Attributes
     ----------
     task_id : uuid.UUID
-        Identificador único da tarefa que foi iniciada.  
-        Usado para correlacionar este evento com a execução da tarefa.
+        Identificador único da tarefa iniciada.  
+        Usado para correlacionar este evento com o ciclo de vida
+        completo da tarefa.
+
+    title : str
+        Nome ou descrição amigável da tarefa que foi iniciada.  
+        Geralmente utilizado para exibição em interfaces de usuário
+        ou logs.
     """
     
     task_id: UUID
+    title: str
