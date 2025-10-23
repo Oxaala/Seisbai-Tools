87a2710331ea6aac3dfff525bb101073bf0f8830
vitor0p9f <dev.vitorpfontes002@gmail.com>
2025-10-23 20:47:16 -0300

    chore(seisbai_tools/pub_sub/decorators): atualiza o decorador Subscribe
    Extrai a lógica de inscrição para a função `apply_subscription_logic`,
    permitindo a criação de mais decoradores que utilizem a mesma lógica
    e evitando duplicação de código.

--- seisbai_tools/pub_sub/decorators/subscribe.py
+++ seisbai_tools/pub_sub/decorators/subscribe.py
@@ -1,27 +1,8 @@
-
-from functools import wraps
-from inspect import signature
-from seisbai_tools.pub_sub.pub_sub import PubSub
-from seisbai_tools.types import Args, Callback, Kwargs
+from seisbai_tools.pub_sub.utils.subscription_logic import apply_subscription_logic
+from seisbai_tools.types import Callback
 
 
 def Subscribe(topic: str):
     def decorator(function: Callback):
-        
-        params = list(signature(function).parameters.keys())
-        is_method = bool(params and params[0] == "self")
-
-        if is_method:
-            function._event_topic = topic
-            
-            return function
-        else:
-            @wraps(function)
-            
-            def wrapper(*args: Args, **kwargs: Kwargs):
-                return function(*args, **kwargs)
-            
-            PubSub().subscribe(topic, wrapper)
-            
-            return wrapper
+        return apply_subscription_logic(topic, function)
     return decorator
