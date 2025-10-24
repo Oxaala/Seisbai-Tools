b034884ab6891bc8388dfb9317eb58ad5c55522f
vitor0p9f <dev.vitorpfontes002@gmail.com>
2025-10-24 18:32:41 -0300

    chore(sesibai_tools/pub_sub/decorators): exporta os decorators PubSubThread e PubSubMethodAnnotation

--- seisbai_tools/pub_sub/decorators/__init__.py
+++ seisbai_tools/pub_sub/decorators/__init__.py
@@ -1,3 +1,5 @@
 from .event_listener import EventListener
 from .command_handler import CommandHandler
 from .subscribe import Subscribe
+from .pub_sub_method_annotation import PubSubMethodAnnotation
+from .pub_sub_thread import PubSubThread
