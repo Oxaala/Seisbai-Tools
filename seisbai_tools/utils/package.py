36046e351d85bdc72cfaaa8d642bf0d51182d04f
vitor0p9f <dev.vitorpfontes002@gmail.com>
2025-10-28 10:56:36 -0300

    fix(seisbai_tools/utils): corrige importação circular
    Remove a importação da classe `Base` do arquivo `package.py`.

--- seisbai_tools/utils/package.py
+++ seisbai_tools/utils/package.py
@@ -2,7 +2,6 @@ from datetime import datetime, timezone
 from typing import Any, Dict
 from uuid import UUID, uuid4
 from msgspec import Struct, field
-from ..eda.base import Base
 from ..utils.msgspec import dict_to_struct, struct_to_dict
 
 from ..utils.get_class_type import get_type
@@ -14,11 +13,11 @@ class _Payload(Struct):
     data: Dict[str, Any]
 
     @classmethod
-    def from_object(cls, obj: Base) -> "_Payload":
+    def from_object(cls, obj: object) -> "_Payload":
         """Cria payload a partir de um objeto comum"""
         return cls(type_path=get_import_path(type(obj)), data=struct_to_dict(obj))
 
-    def reconstruct(self) -> Base:
+    def reconstruct(self) -> Any:
         """Reconstrói o objeto original"""
         cls = get_type(self.type_path)
         return cls(**dict_to_struct(self.data))
@@ -30,5 +29,5 @@ class Package(Struct):
     timestamp: datetime = field(default_factory=lambda: datetime.now(timezone.utc))
     
     @classmethod
-    def wrap(cls, obj: Base) -> "Package":
+    def wrap(cls, obj: object) -> "Package":
         return cls(payload=_Payload.from_object(obj))
