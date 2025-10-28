2ac72d38d4b8b26d9884c9479ee7da59477d09f4
vitor0p9f <dev.vitorpfontes002@gmail.com>
2025-10-28 14:18:49 -0300

    chore(seisbai_tools/eda/commands): adiciona campo opcional de correlation_id para a classe de comando

--- seisbai_tools/eda/commands/command.py
+++ seisbai_tools/eda/commands/command.py
@@ -1,14 +1,22 @@
+from typing import Optional
+
+from msgspec import field
+from ulid import ULID
 from ..base import Base
 
 
 class Command(Base, frozen=True, kw_only=True):
     """
-    Representa um **comando** no sistema.
+    Representa um comando no sistema.
 
     Em arquiteturas baseadas em mensageria, um comando é uma instrução explícita
     enviada para que o sistema realize uma ação. Diferente de um evento, o comando
-    descreve **intenção de alteração de estado futura**.
+    descreve intenção de alteração de estado futura.
+
+    Comandos que iniciam um novo fluxo de operação podem omitir o `correlation_id`.
+    Já comandos que pertencem a um fluxo existente devem possuir um
+    `correlation_id` válido, herdado do comando inicial.
 
     Herda de :class:`Base`.
     """
-    pass
+    correlation_id: Optional[ULID] = field(default=None)
