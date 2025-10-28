fd82ecb51c5c19769c3428024889e7ee0b9fb0ec
vitor0p9f <dev.vitorpfontes002@gmail.com>
2025-10-28 09:45:03 -0300

    feat(seisbai_tools/utils): adiciona classe para transferência de dados
    Adiciona a classe `Package`, permitindo a transferência e recuperação
    das subclasses de `Base` entre os sistemas que compartilham esta
    biblioteca.

---
+++ seisbai_tools/seisbai_tools/utils/package.py
@@ -0,0 +1,34 @@
+from datetime import datetime, timezone
+from typing import Any, Dict
+from uuid import UUID, uuid4
+from msgspec import Struct, field
+from ..eda.base import Base
+from ..utils.msgspec import dict_to_struct, struct_to_dict
+
+from ..utils.get_class_type import get_type
+from ..utils.import_path import get_import_path
+
+
+class _Payload(Struct):
+    type_path: str
+    data: Dict[str, Any]
+
+    @classmethod
+    def from_object(cls, obj: Base) -> "_Payload":
+        """Cria payload a partir de um objeto comum"""
+        return cls(type_path=get_import_path(type(obj)), data=struct_to_dict(obj))
+
+    def reconstruct(self) -> Base:
+        """Reconstrói o objeto original"""
+        cls = get_type(self.type_path)
+        return cls(**dict_to_struct(self.data))
+
+
+class Package(Struct):
+    payload: _Payload
+    id: UUID = field(default_factory=uuid4)
+    timestamp: datetime = field(default_factory=lambda: datetime.now(timezone.utc))
+    
+    @classmethod
+    def wrap(cls, obj: Base) -> "Package":
+        return cls(payload=_Payload.from_object(obj))
