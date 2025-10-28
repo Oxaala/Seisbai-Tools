6f807ec8897650ce05bab2119e9d1f9ac17a7930
vitor0p9f <dev.vitorpfontes002@gmail.com>
2025-10-28 09:57:11 -0300

    chore(seisbi_tools/eda/events): reanomeando arquivo do evento de atualização de progresso

--- seisbai_tools/seisbai_tools/eda/events/__init__.py
+++ seisbai_tools/seisbai_tools/eda/events/__init__.py
@@ -3,6 +3,6 @@ from .canceled import CanceledEvent
 from .completed import CompletedEvent
 from .failed import FailedEvent
 from .paused import PausedEvent
-from .progress_updatedevent import ProgressUpdatedEvent
+from .progress_updated import ProgressUpdatedEvent
 from .resumed import ResumedEvent
 from .started import StartedEvent
